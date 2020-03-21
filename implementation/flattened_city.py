99fe71f5b927b22aa85ab6c5d8398441f428500c
NADEE KHAN <nkhan@localhost.localdomain>
2020-03-21 11:09:00 +0530

    added solution to 3d_surface_area, absolute_permutation, flattened_city, lady_bug

--- implementation/flattened_city.py
+++ implementation/flattened_city.py
@@ -5,36 +5,34 @@ cities_with_space_station = sorted( [int(i) for i in str(input()).split(" ")] )
 
 #print("cities_with_space_station: ", cities_with_space_station)
 
+cities_without_space_station = []
 
 longest_chain = []
-chains = []
-
+cities = []
+last_space_station = -1
 for i in range(0,len(cities_with_space_station)):
-    if len(cities_with_space_station) == 1 and cities_with_space_station[i] != 0:
-        longest_chain.append(len(range(0, cities_with_space_station[i])))
-    elif i == len(cities_with_space_station) -1:
+    longest_chain.append(len(range(last_space_station+1, cities_with_space_station[i])))
+
+
+    if i == len(cities_with_space_station) -1:
+        #longest_chain.append(len(range(last_space_station+1, cities_with_space_station[i])))
         longest_chain.append(len(range(cities_with_space_station[i]+1, n)))
-    elif i == 0 and cities_with_space_station[i] == 0:
-        longest_chain.append(len(range(0, cities_with_space_station[i])))
-        longest_chain.append(len(range(cities_with_space_station[i]+1, cities_with_space_station[i+1])))
-    elif i == 0:
-        longest_chain.append(len(range(0, cities_with_space_station[i])))
-    else:
-        longest_chain.append(len(range(cities_with_space_station[i]+1, cities_with_space_station[i+1])))
 
+    last_space_station = cities_with_space_station[i]
 
-#longest_chain = sorted(longest_chain)
-    
 #print(longest_chain)
+
+
 max_val = max(longest_chain)
 
 
 
 if max_val == longest_chain[-1] or max_val == longest_chain[0]:
     print(max_val)
+elif (max_val + 1)//2 < longest_chain[-1] or (max_val + 1)//2 < longest_chain[0]:
+    print(max([longest_chain[-1], longest_chain[0]]))
 else:
     print((max_val + 1)//2)
-
 
     
 
